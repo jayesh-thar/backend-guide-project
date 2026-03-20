68b7fa4a1b50f573dca02b548a5d3df077222833
Jayesh Thar <jayeshthar1409@gmail.com>
2026-03-20 13:04:54 +0530

    add profile routes

--- auth/src/routes/auth.route.js
+++ auth/src/routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { register, login, logout } from '../controllers/auth.controller.js'
+import { register, login, logout } from "../controllers/auth.controller.js";
+import verifyToken from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
-
 
 /* # Status Code:
         200 - Success
@@ -13,17 +13,30 @@ const router = express.Router();
         500 - Server error
 */
 
-
 // @desc    Register a new user
 // @route   POST /api/auth/register
 // @access  Public (anyone can hit this route)
-router.post("/register", register); 
-// userRegister() runs immediately when server starts — before any request even comes in. That's wrong. Route needs a reference to the function — not the result of calling it. You're saying — "when someone hits /register, THEN run this function" Not — "run this function right now while setting up routes"
+router.post("/register", register);// register() runs immediately when server starts — before any request even comes in. That's wrong. Route needs a reference to the function — not the result of calling it. You're saying — "when someone hits /register, THEN run this function" Not — "run this function right now while setting up routes"
 
-router.post("/login", login)
+// @desc    login a new user
+// @route   POST /api/auth/login
+// @access  Public
+router.post("/login", login);
 
-router.post('/logout', logout);
+// @desc    logout a new user
+// @route   POST /api/auth/logout
+// @access  Public
+router.post("/logout", logout);
 
 
+// Protected routes:
+
+
+router.get("/profile", verifyToken, (req, res) => {
+        res.json({
+                success: true,
+                userId: req.userId
+        })
+});
 
 export default router;
